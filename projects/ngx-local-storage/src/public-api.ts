37c0eaa8bb1d964156ad888ebd525f2584c969b4
Newteq <newt.fourie@entelect.co.za>
2019-05-22 16:29:27 +0200

    fix the public api

--- projects/ngx-local-storage/src/public-api.ts
+++ projects/ngx-local-storage/src/public-api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of ngx-local-storage
  */
 
-export * from './lib/ngx-local-storage.service';
+export * from './lib/local-storage.service';
 export * from './lib/ngx-local-storage.module';
