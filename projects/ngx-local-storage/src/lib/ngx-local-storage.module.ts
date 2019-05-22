86f0be9dec0b38539d655de45f40b54edc98e3d1
Newteq <newt.fourie@entelect.co.za>
2019-05-22 16:23:51 +0200

    Update the module to allow us to configure it forRoot

--- projects/ngx-local-storage/src/lib/ngx-local-storage.module.ts
+++ projects/ngx-local-storage/src/lib/ngx-local-storage.module.ts
@@ -1,8 +1,40 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
+import { NgxLocalStorageConfig } from './config/ngx-local-storage-config';
+import { ConfigToken } from './config/config-token';
+import { NgxLocalStorageService } from './ngx-local-storage.service';
+
+export function provideStorageService(config: NgxLocalStorageConfig): NgxLocalStorageService {
+  return new NgxLocalStorageService(config);
+}
 
 @NgModule({
   declarations: [],
   imports: [],
   exports: [],
 })
-export class NgxLocalStorageModule { }
+export class NgxLocalStorageModule {
+	static forRoot(config? : NgxLocalStorageConfig): ModuleWithProviders {
+		return {
+			ngModule: NgxLocalStorageModule,
+			providers: [
+				{
+					provide: ConfigToken,
+					useValue: config,
+				},
+				{
+					provide: NgxLocalStorageService,
+					useFactory: provideStorageService,
+					deps: [
+						ConfigToken,
+					]
+				}
+			]
+		};
+	}
+
+	constructor(@Optional() @SkipSelf() parentModule: NgxLocalStorageModule) {
+    if (parentModule) {
+      throw new Error('NgxLocalStorageModule is already loaded. Please make sure that this is only imported once (usually via the AppModule)');
+    }
+  }
+}
