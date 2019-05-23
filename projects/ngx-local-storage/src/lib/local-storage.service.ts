6498f74cf629b3d1db8fd45f73dad2b530c28deb
Newteq <newt.fourie@entelect.co.za>
2019-05-23 14:25:44 +0200

    make some improves to this service to retrieve strongly typed and not use prefix

--- projects/ngx-local-storage/src/lib/local-storage.service.ts
+++ projects/ngx-local-storage/src/lib/local-storage.service.ts
@@ -11,6 +11,8 @@ export class LocalStorageService {
 	private readonly prefix: string;
 	private readonly convertToFromJson: boolean;
 
+	private readonly usePrefix: boolean;
+
 	constructor(@Inject(ConfigToken) config?: NgxLocalStorageConfig) {
 		if (config) {
 			this.prefix = config.prefix || defaultConfig.prefix;
@@ -19,32 +21,43 @@ export class LocalStorageService {
 			this.prefix = defaultConfig.prefix;
 			this.convertToFromJson = defaultConfig.defaultJsonConversion;
 		}
+
+		if (this.prefix === '' || this.prefix === null || this.prefix === undefined) {
+			this.usePrefix = false;
+		} else {
+			this.usePrefix = true;
+		}
 	}
 
-	public setItem(key: string, value: any, toJson: boolean = this.convertToFromJson) {
+	public setItem(key: string, value: any, toJson: boolean = this.convertToFromJson): void {
 		let valueToStore: any;
 		if (toJson) {
 			valueToStore = JSON.stringify(value);
 		} else {
 			valueToStore = value;
 		}
-		localStorage.setItem(`${this.prefix}${key}`, valueToStore);
+
+		localStorage.setItem(`${this.getKey(key)}`, valueToStore);
 	}
 
-	public getItem(key: string, fromJson: boolean = this.convertToFromJson) {
-		const value = localStorage.getItem(`${this.prefix}${key}`);
+	public getItem<T = any>(key: string, fromJson: boolean = this.convertToFromJson): T | any {
+		const value = localStorage.getItem(`${this.getKey(key)}`);
 		if (fromJson) {
-			return JSON.parse(value);
+			return <T> JSON.parse(value);
 		} else {
 			return value;
 		}
 	}
 
-	public removeItem(key: string) {
-		localStorage.removeItem(`${this.prefix}${key}`);
+	public removeItem(key: string): void {
+		localStorage.removeItem(`${this.getKey(key)}`);
 	}
 
-	public clear() {
+	public clear(): void {
 		localStorage.clear();
+	}
+
+	private getKey(key: string): string {
+		return this.usePrefix ? `${this.prefix}${key}` : key;
 	}
 }
